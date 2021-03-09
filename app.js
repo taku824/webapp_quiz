1e1b8a763bd51c610da37c98fd8df816d0127330
Fujisaki Taku <74420472+taku824@users.noreply.github.com>
2021-03-09 23:14:00 +0900

    問題の追加５問、解説、称号

--- app.js
+++ app.js
@@ -1,38 +1,61 @@
 const quiz = [
   {
-    qusetion: 'ゲーム市場、最も売れたゲーム機は次のうちどれ？',
+    qusetion: '地球上でもっとも血圧が高い動物は？',
     answers: [
-    'スーパファミコン',
-    'プレステーション２',
-    '任天度スイッチ',
-    'ニンテンドーDS'
+    '人間',
+    'チンパンジー',
+    'ゾウ',
+    'キリン'
     ],
-    correct: 'ニンテンドーDS'
+    correct: 'キリン',
+    comment: '人間の最高血圧が、健康な人の場合120mmHg前後なのに対し、キリンの場合、心臓に最も近いところで約260mmHgにもなるという。'
   },{
-    qusetion: '糸井重里が企画に関わった、任天堂の看板ゲームといえば？',
+    qusetion: 'ゴリ押しの「ゴリ」ってなんのゴリ？',
     answers: [
-    'MOTHERS2',
-    'スーパーマリオブラザーズ３',
-    'スーパドンキーコング',
-    '星のカービィ'
+    'ゴリラ',
+    '懲り懲り',
+    '小魚のゴリ',
+    'ゴリゴリという音'
     ],
-    correct: 'MOTHERS2'
+    correct: '小魚のゴリ',
+    comment: '石にへばりついたゴリを追い込む「ゴリ押し漁」の様子から、「ゴリ押し」という言葉が生まれたという。'
   },{
-    qusetion: 'FFIVの主人公の名前は？',
+  	qusetion: 'オーストラリアの世界遺産「エアーズロック」の地上に出ている部分の割合は？',
     answers: [
-    'フリオニール',
-    'クラウド',
-    'ティーダ',
-    'セシル'
+    '1%未満',
+    '約５％',
+    '約５０％',
+    '約１００％'
     ],
-    correct: 'セシル'
+    correct: '約５％',
+    comment: '残り９５％は数キロに渡り地中に埋まっていると言われている。'
+  },{
+  	qusetion: '日本で一番多いものは？',
+    answers: [
+    '電車の駅',
+    '病院',
+    '歯医者',
+    'コンビニ'
+    ],
+    correct: '歯医者',
+    comment: '歯科医院の数は6万8872軒、コンビニの店舗数は5万5322軒（2017年調査）'
+  },{
+    qusetion: '自動販売機で10円玉は何枚まで使える？',
+    answers: [
+    '10枚',
+    '20枚',
+    '30枚',
+    '50枚以上'
+    ],
+    correct: '20枚',
+    comment: 'たとえば、駅券売機で210円の切符を購入する場合、10円玉21枚では購入できない。これは、同一硬貨が強制力をもって使えるのは20枚までと定めた「通貨の単位及び貨幣の発行等に関する法律第7条」に則っているためで、21枚目を入れた途端にシステムがロックされ、硬貨がすべて返却口に戻ってくるよう、制限機能が設定されている。'
   }
 ];
 
 const quizLength = quiz.length;
 let quizIndex = 0;
 let score = 0;
-
+let level = ''
 
 const $button = document.getElementsByTagName('button');
 const buttonLength = $button.length;
@@ -52,10 +75,10 @@ setQuiz();
 
 const clickHandler = (e) => {
   if(quiz[quizIndex].correct === e.target.textContent){
-      window.alert('正解！');
+      window.alert('正解！' + quiz[quizIndex].comment);
       score++;
     }else{
-      window.alert('不正解！');
+      window.alert('不正解！正解は' + quiz[quizIndex].correct + '!' + quiz[quizIndex].comment);
     }
 
     quizIndex++;
@@ -64,7 +87,10 @@ const clickHandler = (e) => {
       setQuiz();
     }else{
       // 結果画面へ
-      window.alert('終了！あなたの正解数は' + score + '/' + quizLength + 'です！');
+    	if(score > 4){
+    		level = 'すごい！雑学博士だ！';
+      	}
+      window.alert('終了！あなたの正解数は' + score + '/' + quizLength + 'です！' + level);
     }
 
 };
